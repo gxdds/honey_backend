ab066bb3ff0d488aeadd207cf466bad7c1cabf0f
Vinícius Gadioli <viniciusgadioli2@gmail.com>
2025-01-11 21:04:22 -0300

    Update test.js

--- api/test.js
+++ api/test.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   try {
     const result = await sql`SELECT version()`;
     const { version } = result[0];
-    res.status(200).send(version);
+    res.status(200).send(version + "\n deu certo caralho kkkkkkkkkkkk");
   } catch (error) {
     console.error("Erro ao conectar ao banco:", error);
     res.status(500).send("Erro interno do servidor");
